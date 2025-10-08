0cae1d186f2e9e7f80a260a060b098b9aa734858
Davide De Rosa <keeshux@gmail.com>
2025-10-08 06:47:09 +0000

    0.0.20250630 (3)

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 
 let filename = "wg-go.xcframework.zip"
-let version = "0.0.2025063102"
-let checksum = "2596432d878c7132b450f4e21a40c8e1a3c15b05a7049d3b5d7e50f982dc27a0"
+let version = "0.0.2025063103"
+let checksum = "de4985051c617cb504267cf0e238dc97773a81777d63c9318d175f87a11331b2"
 
 let package = Package(
     name: "wg-go-apple",
